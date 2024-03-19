29d69f706c4e13376450c9c47c6d4511588ce1be
Prateek Shaily <prateekshaily@gmail.com>
2024-03-19 15:15:01 +0530

    ..

--- about.js
+++ about.js
@@ -15,18 +15,18 @@ window.addEventListener("scroll", function () {
   prevScrollPos = currentScrollPos;
 });
 
-// Validate the contact form 
+// Validate the form 
 document
   .getElementById("contact-form")
   .addEventListener("submit", function (event) {
     event.preventDefault();
-    // Form validation logic here
+    // Form validation 
     let email = document.getElementById("email").value;
     let name = document.getElementById("name").value;
     let message = document.getElementById("message").value;
 
     if (validateEmail(email) && name.trim() !== "" && message.trim() !== "") {
-      // Form submission logic here
+      // Form submission 
       console.log("Form submitted successfully");
       // Reset the form
       document.getElementById("contact-form").reset();
@@ -36,7 +36,7 @@ document
   });
 
 function validateEmail(email) {
-  // Regular expression for email validation
+  // Regular expression for email
   const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return emailPattern.test(email);
 }
