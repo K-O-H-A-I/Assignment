12c722676da85b7745fbf95d427bc2ec597dadf6
Prateek Shaily <prateekshaily@gmail.com>
2024-04-17 15:51:22 +0530

    Adding nodejs

---
+++ app.js
@@ -0,0 +1,156 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
+const ejs = require("ejs");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("./public"));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "view"));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.get("/index", (req, res) => {
+  res.render("index");
+});
+app.get("/service", (req, res) => {
+  res.render("service");
+});
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "QwErTy2468!#%&(",
+  database: "Readers_Retreat",
+});
+
+console.log("Connection:" + connection);
+
+// Serve static files (HTML, CSS, JavaScript)
+app.use(express.static(__dirname));
+
+connection.connect((err) => {
+  if (err) {
+    console.error("Error connecting to database", err);
+  }
+  console.log("Connection confirmed");
+});
+
+// send signin data to database
+app.post("/signSubmit", (req, res) => {
+  const password = req.body["password"];
+  const name = req.body["username"];
+  const email = req.body["email"];
+  const query = "INSERT INTO users (email, name, password) VALUES (?,?,?)";
+
+  console.log(email);
+  console.log(name);
+
+  if (validate_input_signup(email, name, password)) {
+    connection.query(query, [email, name, password], (err, results) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+    res.redirect("/index");
+  } else {
+    console.log("Input validation failed");
+    res.redirect("/login"); // Redirect back to login page or display an error message
+  }
+});
+
+// Check for login data from database
+app.post("/loginSubmit", (req, res) => {
+  const password = req.body["login-password"];
+  const name = req.body["login-username"];
+
+  console.log("2");
+
+  // Validate user input
+  if (validate_input_login(name, password)) {
+    // Pass empty string for email
+    // Match input data with the database to check if the user is logged in
+    const query = "SELECT * FROM users WHERE name = ? AND password = ?";
+    connection.query(query, [name, password], (err, results) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Error accessing database");
+      } else {
+        if (results.length > 0) {
+          // User is logged in, redirect to index page
+          res.redirect("/index");
+        } else {
+          // User not found
+          res.redirect("/index");
+        }
+      }
+    });
+  } else {
+    // Invalid input, redirect to login page with error message
+    res.redirect("/index");
+  }
+});
+
+
+//funxtion to validate login and signup forms
+function validate_input_signup(email, name, password) {
+  console.log(email);
+  // Regular expression patterns for validation
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const namePattern = /^[a-zA-Z0-9]+$/;
+  const passwordPattern = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-zA-Z]).{8,}$/;
+
+  // Check if email is valid
+  if (!emailPattern.test(email)) {
+    console.log("Invalid email address");
+    return false;
+  }
+  // Check if username is valid
+  if (!namePattern.test(name)) {
+    console.log("Invalid username");
+    return false;
+  }
+  // Check if password is valid
+  if (!passwordPattern.test(password)) {
+    console.log(
+      "Password must be at least 8 characters long and contain at least one letter and one number"
+    );
+    return false;
+  }
+  // All inputs are valid
+  return true;
+}
+
+function validate_input_login(name, password) {
+  // Regular expression patterns for validation
+  const namePattern = /^[a-zA-Z0-9]+$/;
+  const passwordPattern = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-zA-Z]).{8,}$/;
+
+  // Check if username is valid
+  if (!namePattern.test(name)) {
+    console.log("Invalid username");
+    return false;
+  }
+  // Check if password is valid
+  if (!passwordPattern.test(password)) {
+    console.log(
+      "Password must be at least 8 characters long and contain at least one letter and one number"
+    );
+    return false;
+  }
+  return true; // All validation checks passed
+}
+
+// Start server
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
---
+++ public/js/home.js
@@ -0,0 +1,153 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // To access the login and sign up buttons
+  const loginButton = document.querySelector(".login");
+  const signupButton = document.querySelector(".signup");
+
+  // To access the login and sign up popups
+  const loginPopup = document.getElementById("loginPopup");
+  const signupPopup = document.getElementById("signupPopup");
+
+  // Function to open a popup and close any already opened popup
+  function openPopup(popup) {
+    // Close any already opened popup
+    if (popup === loginPopup && signupPopup.style.display === "block") {
+      signupPopup.style.display = "none";
+    } else if (popup === signupPopup && loginPopup.style.display === "block") {
+      loginPopup.style.display = "none";
+    }
+
+    // Open the selected popup
+    popup.style.display = "block";
+  }
+
+  // To handle the display of the login popup
+  loginButton.addEventListener("click", function () {
+    openPopup(loginPopup);
+  });
+
+  // To handle the display of the signup popup
+  signupButton.addEventListener("click", function () {
+    openPopup(signupPopup);
+  });
+
+  // Close the login popup when the cancel button is clicked
+  document
+    .getElementById("login-cancelbtn")
+    .addEventListener("click", function () {
+      loginPopup.style.display = "none";
+    });
+
+  // Close the signup popup when the cancel button is clicked
+  document
+    .getElementById("signup-cancelbtn")
+    .addEventListener("click", function () {
+      signupPopup.style.display = "none";
+    });
+});
+
+// Check the login-form
+document
+  .getElementById("login-submit")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+
+    // Get email, username and password from form
+    var username = document.getElementById("login-username").value;
+    var password = document.getElementById("login-password").value;
+
+    // Create a regular expression for the default values of email and password
+    const passwordRegex = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-zA-Z]).{8,}$/;
+
+    //Check for the default value
+    if (username.trim() === "" || password.trim() === "") {
+      // Display error message
+      alert("Please enter username and password");
+    } else if (password.length < 8 || !passwordRegex.test(password)) {
+      // Check if password is at least 8 characters long and contains at least one number and one symbol
+      alert(
+        "Password must be at least 8 characters long and contain at least one number and one symbol"
+      );
+    } else {
+      fetch("/loginSubmit", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          UserName: username,
+          UserPassword: password,
+        }),
+      }).then((respopnse) => {
+        if (respopnse.ok) {
+          console.log("login sent successfully");
+        }
+      });
+    }
+  });
+
+// Check the signup form
+document
+  .getElementById("signup-submit")
+  .addEventListener("click", function (event) {
+    event.preventDefault(); // Prevent form submission
+
+    // Get email, username and password from form
+    var username = document.getElementById("signup-username").value;
+    var password = document.getElementById("signup-password").value;
+    var email = document.getElementById("signup-email").value;
+    
+    // Create a regular expression for the default values of email and password
+    const passwordRegex = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-zA-Z]).{8,}$/;
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    //Check for the default value
+    if (
+      username.trim() === "" ||
+      password.trim() === "" ||
+      email.trim() === ""
+    ) {
+      // Display error message
+      alert("Please enter email, username and password");
+    } else if (password.length < 8 || !passwordRegex.test(password)) {
+      // Check if password is at least 8 characters long and contains at least one number and one symbol
+      alert(
+        "Password must be at least 8 characters long and contain at least one number and one symbol"
+      );
+    } else if (!emailPattern.test(email)) {
+      // Check if email is valid
+      alert("Please enter a valid email address.");
+    } else {
+      fetch("/signSubmit", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          userName: username,
+          userPassword: password,
+          userEmail: email,
+        }),
+      }).then((response) => {
+        if (response.ok) {
+          console.log("signin sent successfully");
+        }
+      });
+    }
+  });
+
+// Menu scroll animation
+let prevScrollPos = window.scrollY; // Initial scroll position
+
+window.addEventListener("scroll", function () {
+  let currentScrollPos = window.scrollY;
+
+  if (prevScrollPos > currentScrollPos) {
+    document.querySelector("header").style.top = "0";
+    document.querySelector(".menu").style.top = "0";
+  } else {
+    document.querySelector("header").style.top = "-80px";
+    document.querySelector(".menu").style.top = "-80px";
+  }
+
+  prevScrollPos = currentScrollPos;
+});
